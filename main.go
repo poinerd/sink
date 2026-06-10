00797c8697fe96819b1e65bb18e3afcf8ffd9058
Obadofin Emmanuel <obadofinadedapo123@gmail.com>
2026-06-10 12:02:15 +0100

    yeah

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
+type formResponseType map[string]any // composite type
+
+// controller
+
+func submitForm(w http.ResponseWriter, r *http.Request){
+	var formDetails formResponseType
+	json.NewDecoder(r.Body).Decode(&formDetails)
+	fmt.Println(formDetails)
+}
 
 func main(){
-	
+	http.HandleFunc("/submit", submitForm)
+	fmt.Println("server is up and running on port 8080")
+	http.ListenAndServe(":8000", nil)
 }
+
